9bd047217adad373e69596ad12a07c4115e0c361
Cameroncbrownjr <cameroncbrownjr@gmail.com>
2019-07-03 14:37:29 -0400

    working counter

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,16 +3,8 @@ var targetNumber = Math.floor((Math.random() * 100) + 20);
 $("#number-to-guess").text(targetNumber);
 
 var counter = 0;
-//var numberOptions = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-//var randomItem = numberOptions[Math.floor(Math.random() * numberOptions.length)];
-//for (var i = 0; i < numberOptions.length; i++) {
-    //var imageCrystal = $("<img>");
-    //imageCrystal.addClass("crystalPic");
-    //imageCrystal.attr("src", "https://i.vimeocdn.com/video/469249664_640.jpg");
-    //imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-    //$("#crystals").append(imageCrystal);
-//}
-
+var wins = 0;
+var losses = 0;
 //generate random number and assign value to crystal number one
 var randomnum1 = Math.floor(Math.random() * 12) + 1;
 $("#crystalPicOne").attr("data-crystalvalue", randomnum1);
@@ -25,20 +17,61 @@ $("#crystalPicThree").attr("data-crystalvalue", randomnum3);
 
 var randomnum4 = Math.floor(Math.random() * 12) + 1;
 $("#crystalPicFour").attr("data-crystalvalue", randomnum4);
+console.log(counter);
 
-
-$(".crystalPic").on("click", function() {
+$(".image").on("click", function () {
+    console.log(counter);
     var crystalValue = ($(this).attr("data-crystalvalue"));
     crystalValue = parseInt(crystalValue);
     counter += crystalValue;
-    alert("New score: " + counter);
+    console.log(counter);
+    //alert("New score: " + counter);
+    $("#currentScore").text(counter);
 
     if (counter === targetNumber) {
-      alert("You win!");
+        wins++;
+        $("#wins").text(wins);
+        var targetNumber = Math.floor((Math.random() * 100) + 20);
+        //replicate this for every counter
+        $("#number-to-guess").text(targetNumber);
+
+        counter = 0;
+
+        //generate random number and assign value to crystal number one
+        var randomnum1 = Math.floor(Math.random() * 12) + 1;
+        $("#crystalPicOne").attr("data-crystalvalue", randomnum1);
+
+        var randomnum2 = Math.floor(Math.random() * 12) + 1;
+        $("#crystalPicTwo").attr("data-crystalvalue", randomnum2);
+
+        var randomnum3 = Math.floor(Math.random() * 12) + 1;
+        $("#crystalPicThree").attr("data-crystalvalue", randomnum3);
+
+        var randomnum4 = Math.floor(Math.random() * 12) + 1;
+        $("#crystalPicFour").attr("data-crystalvalue", randomnum4);
     }
 
     else if (counter >= targetNumber) {
-      alert("You lose!!");
+        losses++;
+        $("#losses").text(losses);
+        var targetNumber = Math.floor((Math.random() * 100) + 20);
+        //replicate this for every counter
+        $("#number-to-guess").text(targetNumber);
+
+        counter = 0;
+
+        //generate random number and assign value to crystal number one
+        var randomnum1 = Math.floor(Math.random() * 12) + 1;
+        $("#crystalPicOne").attr("data-crystalvalue", randomnum1);
+
+        var randomnum2 = Math.floor(Math.random() * 12) + 1;
+        $("#crystalPicTwo").attr("data-crystalvalue", randomnum2);
+
+        var randomnum3 = Math.floor(Math.random() * 12) + 1;
+        $("#crystalPicThree").attr("data-crystalvalue", randomnum3);
+
+        var randomnum4 = Math.floor(Math.random() * 12) + 1;
+        $("#crystalPicFour").attr("data-crystalvalue", randomnum4);
     }
 
-  });
+});
